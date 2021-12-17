b9ebe8d2e03c5f192463e07d443ba0f4549d9022
pizzi9523 <pizzitolagiuseppe@live.it>
2021-12-17 17:50:13 +0100

    Complete Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,83 @@
 <footer id="site_footer">
-    SITE FOOTER
+    <div class="top-footer container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="links">
+                    <div class="col-3">
+                        <h5>DC COMICS</h5>
+                        <ul>
+                            <li><a href="">Lorem.</a></li>
+                            <li><a href="">Quae?</a></li>
+                            <li><a href="">Necessitatibus?</a></li>
+                            <li><a href="">Velit!</a></li>
+                            <li><a href="">Error.</a></li>
+                            <li><a href="">Autem.</a></li>
+                            <li><a href="">Quam.</a></li>
+                        </ul>
+
+                        <h5>SHOP</h5>
+                        <ul>
+                            <li><a href="">Lorem.</a></li>
+                            <li><a href="">Similique.</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-3">
+                        <h5>DC</h5>
+                        <ul>
+                            <li><a href="">Lorem.</a></li>
+                            <li><a href="">Quae?</a></li>
+                            <li><a href="">Necessitatibus?</a></li>
+                            <li><a href="">Velit!</a></li>
+                            <li><a href="">Error.</a></li>
+                            <li><a href="">Autem.</a></li>
+                            <li><a href="">Quam.</a></li>
+                            <li><a href="">Lorem.</a></li>
+                            <li><a href="">Dignissimos?</a></li>
+                            <li><a href="">Vel!</a></li>
+                            <li><a href="">Ullam?</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-3">
+                        <h5>SITES</h5>
+                        <ul>
+                            <li><a href="">Lorem.</a></li>
+                            <li><a href="">Quae?</a></li>
+                            <li><a href="">Necessitatibus?</a></li>
+                            <li><a href="">Velit!</a></li>
+                            <li><a href="">Error.</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright col-6 pb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, adipisci nulla reprehenderit,
+
+            </div>
+        </div>
+    </div>
+
+    <div class="bottom-footer container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    <div class="sign-up text-center">SIGN-UP NOW!</div>
+                </div>
+
+                <div class="col-6">
+                    <div class="social text-end">
+                        <div>FOLLOW US</div>
+
+                        <img src="{{asset('img/footer-facebook.png')}}" alt="" />
+                        <img src="{{asset('img/footer-twitter.png')}}" alt="" />
+                        <img src="{{asset('img/footer-youtube.png')}}" alt="" />
+                        <img src="{{asset('img/footer-pinterest.png')}}" alt="" />
+                        <img src="{{asset('img/footer-periscope.png')}}" alt="" />
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
 </footer>
